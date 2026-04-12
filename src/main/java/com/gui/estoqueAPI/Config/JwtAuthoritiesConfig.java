2a9d3cdf57e8eebb6354d6f6bed9326a985be353
Guilherme Silva <gsdsousa@rd.com.br>
2026-04-12 17:21:35 -0300

    feat: jwt config

---
+++ src/main/java/com/gui/estoqueAPI/Config/JwtAuthoritiesConfig.java
@@ -0,0 +1,28 @@
+package com.gui.estoqueAPI.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+@Configuration
+public class JwtAuthoritiesConfig {
+
+    @Bean
+    JwtAuthenticationConverter jwtAuthenticationConverter() {
+
+        JwtGrantedAuthoritiesConverter authoritiesConverter =
+                new JwtGrantedAuthoritiesConverter();
+
+        authoritiesConverter.setAuthoritiesClaimName("authorities");
+
+        authoritiesConverter.setAuthorityPrefix("");
+
+        JwtAuthenticationConverter converter =
+                new JwtAuthenticationConverter();
+
+        converter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+
+        return converter;
+    }
+}
